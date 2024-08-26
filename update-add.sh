5d8cd43640494bb8980d90f96f919ea390456305
tigip4r1 <tigip4r1@proton.me>
2024-08-25 20:26:28 -0400

    Update update-add.sh

--- update-add.sh
+++ update-add.sh
@@ -2,3 +2,4 @@ wget https://iptv-org.github.io/iptv/countries/sv.m3u -O add/sv.m3u
 wget https://iptv-org.github.io/iptv/countries/ca.m3u -O add/ca.m3u
 wget https://iptv-org.github.io/iptv/languages/eng.m3u -O add/eng.m3u
 wget https://iptv-org.github.io/iptv/languages/spa.m3u -O add/spa.m3u
+wget http://m3u4u.com/m3u/dqr6ywwvk3hm8j65yx1w -O afteredit.m3u
